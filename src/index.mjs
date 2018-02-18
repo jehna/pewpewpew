a927d1e547d97b7bbd5b404811e46442d56c89d3
Jesse Luoto <jesse.luoto@gmail.com>
2018-02-18 23:22:09 +0200

    Add proper prettierrc

--- src/index.mjs
+++ src/index.mjs
@@ -5,19 +5,15 @@ import fetch from 'node-fetch'
 const rl = readline.createInterface({ input: process.stdin })
 
 const testPromise = something => Promise.resolve('R: ' + something)
-const duckduck = async q => fetch(`https://api.duckduckgo.com/?q=${q}&format=json`).then(res => res.json())
+const duckduck = async q =>
+  fetch(`https://api.duckduckgo.com/?q=${q}&format=json`).then(res =>
+    res.json()
+  )
 
 const onLine = rl.on.bind(rl, 'line')
 const echo = async q => 'C: ' + q
 
-const xyz = createStream(
-  onLine,
-  echo
-)
+const xyz = createStream(onLine, echo)
 
 xyz(console.log)
-compose(
-  xyz,
-  map(c => 'aa => ' + c),
-  console.log
-)
+compose(xyz, map(c => 'aa => ' + c), console.log)
--- src/pew.mjs
+++ src/pew.mjs
@@ -1,34 +1,34 @@
-export const map = iter => cb => v => cb(iter(v));
+export const map = iter => cb => v => cb(iter(v))
 
-export const filter = iter => cb => v => iter(v) && cb(v);
+export const filter = iter => cb => v => iter(v) && cb(v)
 
-const doThing = fn => cb => v => fn(v).then(cb);
+const doThing = fn => cb => v => fn(v).then(cb)
 
 function isAsync(fn) {
-  return fn.constructor.name === "AsyncFunction";
+  return fn.constructor.name === 'AsyncFunction'
 }
 
 export const compose = (...fns) =>
-  fns.reduceRight((b, a) => (isAsync(a) ? v => a(v).then(b) : a(b)));
+  fns.reduceRight((b, a) => (isAsync(a) ? v => a(v).then(b) : a(b)))
 
 export const debounce = time => {
-  let timeout;
+  let timeout
   return cb => v => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => cb(v), time);
-  };
-};
+    clearTimeout(timeout)
+    timeout = setTimeout(() => cb(v), time)
+  }
+}
 
 export const throttle = time => {
-  let canFire = true;
+  let canFire = true
   return cb => v => {
-    if (!canFire) return;
-    cb(v);
-    canFire = false;
+    if (!canFire) return
+    cb(v)
+    canFire = false
     setTimeout(() => {
-      canFire = true;
-    }, time);
-  };
-};
+      canFire = true
+    }, time)
+  }
+}
 
 export const createStream = (...args) => cb => compose(...args, cb)
